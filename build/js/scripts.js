514090f7bcfae2b9799ac119f59a52893f28c830
Element1493 <Element1493@yandex.ru>
2024-04-21 15:29:15 +0300

    mail

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -24,7 +24,53 @@ let array = {
 		"page404-parallax-3":150,
 		"page404-parallax-4":50
 	},
-	"playlist":[{"track":"Gheaghea Vanea","artist":"Graieste Moldovneste","file":"graieste_moldovneste_gheaghea_vanea.mp3"},{"track":"Ma insor la anu-n mai","artist":"Florinel","file":"florinel_ma_insor_la_anu-n_mai.mp3"},{"track":"Nunta","artist":"nicolae guta si sorina","file":"nicolae_guta_si_sorina-nunta.mp3"},{"track":"of mai Ioane tare vreu banane","artist":"Sergiu Vetirla & Natalita Ola","file":"off_mai_Ioane_tare_vreu_banane.mp3"},{"track":"Gigoloo","artist":"Nicolae Guta","file":"Nicolae_Guta_Gigoloo.mp3"},{"track":"LiVe","artist":"Christian Green","file":"christian_green_live.mp3"}]
+	"playlist":[
+		{	"track":"Gheaghea Vanea",
+			"artist":"Graieste Moldovneste",
+			"file":"graieste_moldovneste_gheaghea_vanea.mp3"
+		},
+		{
+			"track":"Ma insor la anu-n mai",
+			"artist":"Florinel",
+			"file":"florinel_ma_insor_la_anu-n_mai.mp3"
+		},
+		{
+			"track":"Nunta",
+			"artist":"nicolae guta si sorina",
+			"file":"nicolae_guta_si_sorina-nunta.mp3"
+		},
+		{
+			"track":"of mai Ioane tare vreu banane",
+			"artist":"Sergiu Vetirla & Natalita Ola",
+			"file":"off_mai_Ioane_tare_vreu_banane.mp3"
+		},
+		{
+			"track":"Gigoloo",
+			"artist":"Nicolae Guta",
+			"file":"Nicolae_Guta_Gigoloo.mp3"
+		},
+		{
+			"track":"LiVe",
+			"artist":"Christian Green",
+			"file":"christian_green_live.mp3"
+		},
+		{
+			"track":"Dulce-i vinul",
+			"artist":"Ro-mania",
+			"file":"dulce-i_vinul_-_ro-maniea.mp3"
+		},
+		{
+			"track":"Dulce-i vinul",
+			"artist":"Ro-mania",
+			"file":"manea.mp3"
+		},
+		{
+			"track":"Dulce-i vinul",
+			"artist":"Ro-mania",
+			"file":"manea.mp3"
+		}
+	]
+	]
 
 };
 
